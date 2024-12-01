ec9968926663eebc786c02fc0b07fd8b9c30128a
online5880 <gjtjqkr1@naver.com>
2024-12-01 18:57:26 +0900

    코드 수

--- app/core/admin.py
+++ app/core/admin.py
@@ -29,9 +29,9 @@ class UserAdmin(BaseUserAdmin):
     )
     readonly_fields = ['last_login']
     add_fieldsets = (
-        (None,{
-            'classes':('wide',),
-            'fields':(
+        (None, {
+            'classes': ('wide',),
+            'fields': (
                 'email',
                 'password1',
                 'password2',
@@ -39,8 +39,9 @@ class UserAdmin(BaseUserAdmin):
                 'is_active',
                 'is_staff',
                 'is_superuser',
-            )
+            ),
         }),
     )
+
 
 admin.site.register(models.User, UserAdmin)
