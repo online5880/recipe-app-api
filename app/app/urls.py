afc7c611f235c0997f21f750f29ee92f91017018
online5880 <gjtjqkr1@naver.com>
2024-12-01 20:41:28 +0900

    계정 생성, 토큰, 본인 API 만들기

--- app/app/urls.py
+++ app/app/urls.py
@@ -18,12 +18,14 @@ from drf_spectacular.views import (
     SpectacularSwaggerView
 )
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path("api/docs/",
          SpectacularSwaggerView.as_view(url_name='api-schema'),
-         name="api-docs")
+         name="api-docs",
+    ),
+    path("api/user/", include('user.urls')),
 ]
